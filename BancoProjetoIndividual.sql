5141eaf5d57491d263c85f8e8cafe6564b27ed5c
JoaoV004 <joao.osilva@sptech.school>
2024-05-26 12:27:32 -0300

    Alterações no frontEnd

---
+++ BancoProjetoIndividual.sql
@@ -0,0 +1,35 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE gol_futuro;
+
+USE gol_futuro;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50) unique,
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	momento DATETIME,
+    fkUsuario int,
+    foreign key (fkUsuario) references usuario(id)
+);
